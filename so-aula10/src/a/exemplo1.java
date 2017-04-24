812f6b4efc85e15f465212570edcdbeef4369c74
RaphaelHeibel <raphaelrheibel@gmail.com>
2017-04-24 11:13:57 -0300

    Add files via upload

---
+++ so-aula10/src/a/exemplo1.java
@@ -0,0 +1,29 @@
+package a;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class exemplo1 {
+
+	public static void main(String[] args) throws IOException {
+		
+		Scanner entrada = new Scanner(new FileInputStream(FileDescriptor.in));
+		
+		PrintWriter saida = new PrintWriter(new FileOutputStream(FileDescriptor.out));
+		
+		saida.println("Digite o seu nome: ");
+		saida.flush();
+		String nome = entrada.nextLine();
+		saida.println("Prazer em conhecê-lo, " + nome +  "!" );
+		saida.flush();
+		
+		entrada.close();
+		saida.close();
+		
+
+	}
+
+}
